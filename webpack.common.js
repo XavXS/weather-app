e8f24d87bcbfcc9b37fd5ef2aa5cf10ac671b780
xav <xavs421@gmail.com>
2022-12-15 18:45:53 -0700

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/img_[hash][ext][query]',
+        },
+      },
+    ],
+  },
+};
